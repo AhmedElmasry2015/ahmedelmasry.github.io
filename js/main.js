787f54c8dee5b46fe6823b56c0624b06ed6093ed
ahmedelmasry2015 <ahmed.saeed.elmasry@gmail.com>
2017-05-12 17:16:09 +0300

    second iteration

--- js/main.js
+++ js/main.js
@@ -14,7 +14,18 @@ $(function(){
         
     }
     });
-
+  $(".b1").hover(function(){
+        $("#div1").fadeToggle(3000);
+    });
+  
+  $(".b2").hover(function(){
+        $("#div2").fadeToggle(3000);
+        
+    });
+  
+  $(".b3").hover(function(){
+        $("#div3").fadeToggle(3000);
+    });
   
 });
  
